ceab0b1497692152836aad4d839329d703a0b66e
Erichsu903 <126964636+Erichsu903@users.noreply.github.com>
2023-06-19 08:51:58 +0800

    Render - 20230619085100

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import os
+os.system("pip install requests")
+os.system("pip install flask")
+os.system("pip install threading")
 import requests
 from flask import Flask, jsonify, request
 from threading import Thread
